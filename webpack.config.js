d3382dba509d769ae743a332ea2f36cbebf68b06
camilaibs <camilaibs@gmail.com>
2018-02-12 20:39:30 -0200

    webpack - carregamento de imagens

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,12 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
       }
     ]
   }
