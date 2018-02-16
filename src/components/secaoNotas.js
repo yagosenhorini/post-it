39658421769b663804bece65037dce5956a9e597
yagosenhorini <yagosenhorini30@gmail.com>
2018-02-16 20:06:11 -0200

    create secaoNotas and implements function

--- src/components/secaoNotas.js
+++ src/components/secaoNotas.js
@@ -3,24 +3,21 @@ import Section from './section';
 import FormNotas from './formNotas';
 // import listaNotas from '../listaNotas';
 
-function criaFormNotas(props) {
-    const props = {
+function criaFormNotas(posicao, props) {
+    const propsForm = {
         posicao: posicao,
         notaAtual: props.listaNotas.pega(posicao),
         editarFormulario: props.editarFormulario,
         adicionarNota: props.adicionarNota,
         removerNota: props.removerNota
     }
-    return React.createElement(FormNotas, props, children);
+    return React.createElement(FormNotas, propsForm);
 }
 
-function SecaoNotas({ listaNotas, editarFormulario, posicao, adicionarNota, removerNota }) {
-    const props = { className: 'notes' };
-    const children = [];
-    for (let i = 0; i < listaNotas.contaTotal(); i++) {
-        let nota = listaNotas.pega(posicao);
-        let formNotas = criaFormNotas(posicao, nota, editarFormulario, adicionarNota, removerNota);
-        children.push(formNotas, propsForm);
-    }
-    return React.createElement(Section, props, children);
+function SecaoNotas({listaNotas, editarFormulario, adicionarNota,removerNota}) {
+    const propsSection = { className: 'notes' };
+    children = props.listaNotas.map((notaAtual, posicao) => criaFormNotas(posicao, props));
+
+
+    return React.createElement(Section, propsSection, children);
 } export default SecaoNotas;
